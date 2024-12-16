dec09e7c3ec8d077076e5c1a4ab88b764a1225f3
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2024-12-16 20:39:22 +0300

    gateway is prepared

---
+++ lab5/gateway/index.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const cors=require('cors');
+const auth_router =require('./routes/auth_router');
+const reservation_router =require('./routes/reservation_router');
+const hotels_router =require('./routes/hotels_router');
+const PORT = process.env.PORT || 5000
+
+const app = express(); 
+app.use((req, res, next) => {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header(
+       "Access-Control-Allow-Methods",
+       "GET, PUT, POST, DELETE, OPTIONS"
+     );
+     res.header(
+       "Access-Control-Allow-Headers",
+       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+     );
+     next();
+   });
+   app.use(cors());
+   app.use(express.json()); 
+   app.use("/auth",auth_router);
+   app.use("/reservation",reservation_router);
+   app.use("/hotels",hotels_router);
+
+const start = async()=> {
+     try{
+          app.listen(PORT,()=>console.log(`server started on port ${PORT}`)); 
+     }
+     catch(e){  
+          console.log(e)    
+     }
+}
+
+start()
+
+
