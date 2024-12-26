17a7e904df8e51fe9014d5ef49fd076df257eb8e
Albert Bairamukov <90350959+Albertoejdhdjfjdjfj@users.noreply.github.com>
2024-12-26 18:22:31 +0300

    lab6 is ready

--- lab6/client/src/redux/actions/state_components/actions.ts
+++
@@ -1,5 +0,0 @@
-import { SET_SEARCH,SET_EMAIL} from './actionsTypes';
-import { createAction } from 'redux-actions';
-
-export const set_search = createAction(SET_SEARCH);
-export const set_email = createAction(SET_EMAIL);
--- lab6/client/src/redux/reducers/combineReducers.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import state_components from './state_components/reducer';
-
-export default combineReducers({
-  state_components
-});
